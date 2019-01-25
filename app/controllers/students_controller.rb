8df68f37cefa088b4fd9a4e10290cc4f07cb4ab5
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-25 12:44:44 -0300

    ADD: Student model and associated controller

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'students/new'
+  get 'students/show'
+  get 'students/create'
+
+  root 'students#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
